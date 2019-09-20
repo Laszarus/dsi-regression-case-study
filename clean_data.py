ebb37a682f9fc44ff68648f2bf4114ce74d0e82a
Bobcat90 <delancey314@netzero.net>
2019-09-20 16:03:28 -0600

    pushing final code

--- clean_data.py
+++ clean_data.py
@@ -1,7 +1,25 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+import numpy as np
+from pandas.plotting import scatter_matrix
+from statistics import mean
 
-df = pd.read_csv('predict_auction_price/data/Train.csv')
+from sklearn.linear_model import LinearRegression, Ridge, Lasso
+from sklearn.model_selection import train_test_split, KFold
+from sklearn.preprocessing import StandardScaler
+from sklearn.base import clone
+
+
+import matplotlib.pyplot as plt
+from predict_auction_price.score_model import rmsle
+
+plt.style.use("ggplot")
+
+# def rmsle(actual, predictions):
+#     log_diff = np.log(predictions+1) - np.log(actual+1)
+#     return np.sqrt(np.mean(log_diff**2))
+
+df = pd.read_csv('predict_auction_price/data/Train.csv', low_memory=False)
 
 df = df[['SalePrice', 'YearMade', 'saledate', 
         'Enclosure','Turbocharged', 'ProductGroup', ]]
@@ -27,3 +45,36 @@ df = df[df.Enclosure != 'None or Unspecified']
 #enclosure group dummies
 enc_dummies = pd.get_dummies(df['Enclosure'], prefix='Enc')
 df = pd.concat([df, enc_dummies], axis = 1)
+
+df.dropna(subset=['SalePrice','age','Enclosure','ProductGroup',],inplace=True)
+
+
+df.drop(['Turbocharged'],axis = 1, inplace=True)
+target_column='SalePrice'
+
+X=df.drop(['SalePrice','YearMade','saleyear','saledate','Enclosure','ProductGroup'],axis=1)
+y=df['SalePrice']
+
+X_train, X_test, y_train, y_test = train_test_split(X, y)
+
+#casting df to arrays
+# X = np.array(X)
+# y = np.array(y)
+
+# kf = KFold(n_splits=10, random_state=42)
+# for train_index, test_index in kf.split(X):
+        
+#         X_train, X_test = X[train_index], X[test_index]
+#         y_train, y_test = y[train_index], y[test_index]
+
+#for train_index, test_index in kfold.split(X_train):
+model = LinearRegression()
+
+model.fit(X_train, y_train)
+y_predict = model.predict(X_test)
+
+# print(f"y_test: {type(y_test)}, y_predict: {type(y_predict)}")
+
+rmsles = [rmsle(y_test, y_predict)]
+# meanrmlse=mean(rmsles)
+print(rmsles)
---
+++ predict_auction_price/auction stuff.py
@@ -0,0 +1,5 @@
+import pandas as import pd
+import numpy as np 
+
+df = pd.read_csv('data/train.csv
+
