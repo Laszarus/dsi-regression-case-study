69afea589ffca363828952197df2f9bb84511839
LucasXavierChurchman <lucasxavierchurchman@gmail.com>
2019-09-20 14:21:22 -0400

    whoops

--- scatmat.py
+++ scatmat.py
@@ -1,11 +1,13 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+from clean_data import clean
 from pandas.plotting import scatter_matrix
 plt.style.use('ggplot')
 
 from sklearn.model_selection import train_test_split
 
-df = pd.read_csv('predict_auction_price/data/CleanTrainData.csv')
+df = clean()
+
 df2 = df.head(10)
 scatter_matrix(df[['SalePrice', 'YearMade']], figsize=(15,15))
 plt.show()
